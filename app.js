926bd8c1cff669f98bc53230b51ff7913ba873b7
0_o <summer.wxy@gmail.com>
2017-12-05 09:30:46 +0800

    fix static serve

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 
 // 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
 const Koa = require('koa');
-
 // 注意require('koa-router')返回的是函数:
 const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser');
@@ -23,8 +22,9 @@ const controller = require('./commons/controller');
 
 // 创建一个Koa对象表示web app本身:
 const app = new Koa();
-
+app.use(require('koa-static-server')({rootDir: 'static', rootPath: '/static'}));
 app.use(bodyParser());
+
 
 // 对于任何请求，app将调用该异步函数处理请求：
 app.use(async (ctx, next) => {
@@ -52,7 +52,7 @@ app.use(async (ctx, next) => {
 })
 
 app.use(controller());
-app.use(require('koa-static-server')({rootDir: 'static', rootPath: '/static'}))
+
 
 
 // 在端口3000监听:
